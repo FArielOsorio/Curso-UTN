17541e0a34d18fc1421c50bdfcf506f00588a057
FArielOsorio <facundoosorio95@gmail.com>
2023-07-28 19:09:20 -0300

    Update

--- JS (MÓDULO 2)/LABORATORIO 01/ACTIVIDADES/ejUno.js
+++ JS (MÓDULO 2)/LABORATORIO 01/ACTIVIDADES/ejUno.js
@@ -1,6 +1,6 @@
 document.write("Ingrese 3 valores");
 
-let a,b,c,d,mayor;
+let a,b,c,d,e,mayor,menor;
 a = parseint(prompt("Ingrese el primer número: "));
 b = parseint(prompt("Ingrese el segundo número: "));
 c = parseint(prompt("Ingrese el tercer número: "));
@@ -20,3 +20,19 @@ else {
 }
 
 document.write("El número más grande es: ",mayor);
+
+if (a<b){
+    e = a;
+}
+else {
+    e = b;
+}
+
+if (e<c){
+    menor = e;
+}
+else {
+    menor = c;
+}
+
+document.write("El número más chico es: ",menor);
